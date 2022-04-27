3c6b5d7ce0e80b90039187c9b86735e498963240
Giulio2008 <giulio.defaveri@student.h-is.com>
2022-04-27 11:22:39 +0200

    .

--- game.js
+++ game.js
@@ -1,12 +1,26 @@
 let lastRenderTime = 0
 
 
+
 function main(currenTime) {
-    constsecondsSinceLastRender = (currenTime - lastRenderTime ) / 1000 
-window.requestAnimationFrame (main)
-console.log(secondsSinceLastRender)
+    window.requestAnimationFrame (main)
+    const secondsSinceLastRender = (currenTime - lastRenderTime ) / 1000 
+     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return 
+console.log('Render')
 lastRenderTime = currenTime
+
+update()
+draw()
+
+} 
+
+window.requestAnimationFrame(main)
+
+function update() {
 
 }
 
-window.requestAnimationFrame(main)
+
+function draw() {
+    
+}
